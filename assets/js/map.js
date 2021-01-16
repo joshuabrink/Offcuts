89df8bb58bbe066bf15973b6d1a72343e7c29554
joshuabrink1 <u19185678@tuks.co.za>
2021-01-16 16:48:19 +0200

    map view

---
+++ assets/js/map.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+const map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([22, 8.82]),
+    zoom: 10
+  })
+})
